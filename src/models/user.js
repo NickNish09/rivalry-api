9f009c4fc3e9780fb7ff5fba9baab6d386367ebd
nicknish09 <nnmarques97@gmail.com>
2020-06-26 20:43:26 -0300

    Prettier Installed

--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,5 @@
-const mongoose = require('../database');
-const bcrypt = require('bcryptjs');
+const mongoose = require("../database");
+const bcrypt = require("bcryptjs");
 const UserSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -18,16 +18,16 @@ const UserSchema = new mongoose.Schema({
   },
   createdAt: {
     type: Date,
-    default: Date.now
+    default: Date.now,
   },
 });
 
-UserSchema.pre('save', async function (next) {
+UserSchema.pre("save", async function (next) {
   const hash = await bcrypt.hash(this.password, 10);
   this.password = hash;
 
   next();
 });
 
-const User = mongoose.model('User', UserSchema);
+const User = mongoose.model("User", UserSchema);
 module.exports = User;
