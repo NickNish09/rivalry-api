a689c46e06f515547ecd16ecabfa709c868b1a88
nicknish09 <nnmarques97@gmail.com>
2020-06-27 20:01:45 -0300

    Added autoimport to controller files

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-require("./app/controllers/authController")(app);
-require("./app/controllers/mainController")(app);
+require("./app/controllers/index")(app);
 
 app.listen(3000);
