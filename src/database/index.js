70a885b30f5946b75669e903dd01087e9297079f
nicknish09 <nnmarques97@gmail.com>
2020-07-15 20:11:17 -0300

    print env for debug

--- src/database/index.js
+++ src/database/index.js
@@ -5,6 +5,11 @@ mongoose.connect("mongodb://mongo:27017/rivalry", {
   useUnifiedTopology: true,
   useCreateIndex: true,
 });
+// mongoose.connect("mongodb://localhost/rivalry", {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   useCreateIndex: true,
+// });
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
