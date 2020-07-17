af5927aea94a42ef75c204acd20e3773e9909547
nicknish09 <nnmarques97@gmail.com>
2020-07-16 23:05:39 -0300

    database production

--- src/database/index.js
+++ src/database/index.js
@@ -1,15 +1,15 @@
 const mongoose = require("mongoose");
 
-// mongoose.connect("mongodb://mongo:27017/rivalry", {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-//   useCreateIndex: true,
-// });
-mongoose.connect("mongodb://localhost/rivalry", {
+mongoose.connect("mongodb://mongo:27017/rivalry", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
 });
+// mongoose.connect("mongodb://localhost/rivalry", {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   useCreateIndex: true,
+// });
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
